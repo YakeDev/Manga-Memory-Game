e63ea13e259cc1ad56ea52d04c035ee9e3917d52
YakeDev <erickay.dev@gmail.com>
2024-11-11 12:41:58 +0200

    Decrease de h of images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,12 +5,14 @@ const Card = (props) => {
 		<div
 			onClick={props.onClick}
 			className='h-full max-w-sm bg-white border border-gray-200 p-2 rounded-3xl  shadow-lg hover:shadow-2xl hover:cursor-pointer transition-shadow delay-300 overflow-hidden '>
-			<img
-				src={props.imgUrl}
-				alt={props.imgAlt}
-				srcSet=''
-				className='rounded-2xl '
-			/>
+			<div className='h-72 overflow-hidden rounded-2xl'>
+				<img
+					src={props.imgUrl}
+					alt={props.imgAlt}
+					srcSet=''
+					className='rounded-2xl'
+				/>
+			</div>
 			<div className='p-2 pb-2'>
 				<p className='mb-2 text-lg leading-tight font-bold text-centerdark:text-white'>
 					{props.imgName}
